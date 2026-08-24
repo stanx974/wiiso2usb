a8848130dfa6acf728cfc046c991680c602fb85f
stanx974 <stanx974@gmail.com>
2026-08-24 21:41:56 +0200

    Delete wii python usb directory

--- wii python usb/WiiConverter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Wii Game Converter - Launcher Linux
-# Lance l'interface graphique par defaut
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR"
-
-python3 wii_converter.py "$@"
